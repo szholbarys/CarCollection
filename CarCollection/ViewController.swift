adc892a161085942469025e6963d4599d438fab6
szholbarys <sakenzholbarys@gmail.com>
2024-11-07 10:50:20 +0500

    Initial Commit

---
+++ CarCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarCollection
+//
+//  Created by Zholbarys on 07.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
